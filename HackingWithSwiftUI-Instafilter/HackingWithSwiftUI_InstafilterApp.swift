ee2d182ad513d8299b3330213ed32069958451fe
Michael Jones <michael.jones90@me.com>
2026-06-27 10:40:34 +0100

    Initial Commit

---
+++ HackingWithSwiftUI-Instafilter/HackingWithSwiftUI_InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackingWithSwiftUI_InstafilterApp.swift
+//  HackingWithSwiftUI-Instafilter
+//
+//  Created by Michael Jones on 27/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct HackingWithSwiftUI_InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
